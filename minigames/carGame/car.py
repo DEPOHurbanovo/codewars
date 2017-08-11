cb0fea29f10f2ec7c2a24c35c4114bb0262f0a2f
m1n0 <mino.lukac@gmail.com>
2017-08-11 20:02:50 +0200

    Grass and wall

--- minigames/carGame/car.py
+++ minigames/carGame/car.py
@@ -3,9 +3,10 @@ pygame.init()
 clock = pygame.time.Clock()
 
 size = width, height = 800, 600
-speed = [1, 1]
+speed = [5, 5]
 black = 0, 0, 0
 green = 0, 255, 0
+grey = 100, 100, 100
 
 car = pygame.image.load('car.png')
 screen = pygame.display.set_mode(size)
@@ -26,13 +27,13 @@ while 1:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_change = -5
+                x_change = -speed[0]
             if event.key == pygame.K_RIGHT:
-                x_change = 5
+                x_change = speed[0]
             if event.key == pygame.K_UP:
-                y_change = -5
+                y_change = -speed[1]
             if event.key == pygame.K_DOWN:
-                y_change = 5
+                y_change = speed[1]
 
 
         if event.type == pygame.KEYUP:
@@ -45,7 +46,20 @@ while 1:
     y += y_change
     car_rect = car_rect.move((x, y))
 
+    # Refresh the screen.
     screen.fill(black)
+    # Grass
+    grass_left = pygame.draw.rect(screen, green, (10, 0, 100, size[1]))
+    grass_right = pygame.draw.rect(screen, green, (size[0] - 10, 0, -100, size[1]))
+    # Wall
+    wall_left = pygame.draw.rect(screen, grey, (0, 0, 10, size[1]))
+    wall_right = pygame.draw.rect(screen, grey, (size[0], 0, -10, size[1]))
+
+    if car_rect.colliderect(grass_left) or car_rect.colliderect(grass_right):
+        speed = [2, 2]
+    else:
+        speed = [5, 5]
+
     screen.blit(car, car_rect)
     pygame.display.update()
     clock.tick(60)
