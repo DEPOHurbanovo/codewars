20e42ad347950b910a6be782fd64dca81d6bc8c8
m1n0 <mino.lukac@gmail.com>
2017-09-22 18:59:56 +0200

    updated motion of car

--- minigames/car-game/car.py
+++ minigames/car-game/car.py
@@ -3,6 +3,7 @@ pygame.init()
 clock = pygame.time.Clock()
 
 size = width, height = 800, 600
+# how many pixels on one move, [x, y].
 speed = [5, 5]
 black = 0, 0, 0
 green = 0, 255, 0
@@ -11,6 +12,7 @@ grey = 100, 100, 100
 car = pygame.image.load('car.png')
 screen = pygame.display.set_mode(size)
 
+# Initialize screen.
 car_rect = car.get_rect()
 car_rect.left = (size[0] / 2) - (car_rect.width / 2)
 car_rect.bottom = size[1]
@@ -19,29 +21,34 @@ x_change = 0
 y_change = 0
 
 while 1:
+    # Reset the stae
     x = 0
     y = 0
-
+    x_change = 0
+    y_change = 0
+    # Check pygame events in every tick.
+    keys_state = pygame.key.get_pressed();
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -speed[0]
-            if event.key == pygame.K_RIGHT:
-                x_change = speed[0]
-            if event.key == pygame.K_UP:
-                y_change = -speed[1]
-            if event.key == pygame.K_DOWN:
-                y_change = speed[1]
+    moving = False
+    if keys_state[pygame.K_LEFT] == True:
+        x_change = -speed[0]
+        moving = True
+    if keys_state[pygame.K_RIGHT] == True:
+        x_change = speed[0]
+        moving = True
+    if keys_state[pygame.K_UP] == True:
+        y_change = -speed[1]
+        moving = True
+    if keys_state[pygame.K_DOWN] == True:
+        y_change = speed[1]
+        moving = True
+    if not moving:
+        x_change = 0
+        y_change = 0
 
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_change = 0
-
+    # Events processed, add change to current position.
     x += x_change
     y += y_change
     car_rect = car_rect.move((x, y))
@@ -55,6 +62,7 @@ while 1:
     wall_left = pygame.draw.rect(screen, grey, (0, 0, 10, size[1]))
     wall_right = pygame.draw.rect(screen, grey, (size[0], 0, -10, size[1]))
 
+    # Collision detection
     if car_rect.colliderect(grass_left) or car_rect.colliderect(grass_right):
         speed = [2, 2]
     else:
@@ -62,4 +70,5 @@ while 1:
 
     screen.blit(car, car_rect)
     pygame.display.update()
+    # Set clock tick to control speed of game
     clock.tick(60)
